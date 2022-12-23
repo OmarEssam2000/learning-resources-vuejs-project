4ee5fda71512bd50552126735f2e342e735bfa47
OmarEssam2000 <74010778+OmarEssam2000@users.noreply.github.com>
2022-12-23 22:14:05 +0200

    Add files via upload

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ['title']
+}
+</script>
+
+<style scoped>
+*{
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+}
+header {
+  width: 100%;
+  height: 5rem;
+  background-color: rgba(2, 101, 250, 0.805);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header h1 {
+  color: white;
+  margin: 0;
+}
+</style>
